023e1c4f73c1d16878e69b4135c9df5f68dddd7d
WooJin <125100704+jinn0135@users.noreply.github.com>
2023-06-08 14:22:01 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,10 +1,6 @@
 def solution(numbers):
-    e2n = {'on':['one','1'],'tw':['two','2'],'th':['three','3'],
-           'fo':['four','4'],'fi':['five','5'],'si':['six','6'],
-           'se':['seven','7'],'ei':['eight','8'],'ni':['nine','9'],
-           'ze':['zero','0']}
-    ans = ''
-    while numbers:
-        ans += e2n[numbers[:2]][1]
-        numbers = numbers[len(e2n[numbers[:2]][0]):]
-    return int(ans)
+    ns = ["zero", "one", "two", "three", "four", "five", 
+          "six", "seven", "eight", "nine"]
+    for i,n in enumerate(ns):
+        numbers = numbers.replace(n,str(i))
+    return int(numbers)
